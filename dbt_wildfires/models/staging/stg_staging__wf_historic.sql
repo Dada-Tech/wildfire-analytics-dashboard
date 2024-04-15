b99749c06944be6132a6cea089127fba9f0a86f9
David <david.olu.dada@gmail.com>
2024-04-15 01:32:42 +0000

    DBT model initialization

---
+++ dbt_wildfires/models/staging/stg_staging__wf_historic.sql
@@ -0,0 +1,67 @@
+with 
+
+source as (
+
+    select * from {{ source('staging', 'wf_historic') }}
+
+),
+
+renamed as (
+
+    select
+        fire_year,
+        fire_number,
+        fire_name,
+        current_size,
+        size_class,
+        fire_location_latitude,
+        fire_location_longitude,
+        fire_origin,
+        general_cause_desc,
+        industry_identifier_desc,
+        responsible_group_desc,
+        activity_class,
+        true_cause,
+        fire_start_date,
+        det_agent,
+        det_agent_type,
+        discovered_date,
+        discovered_size,
+        reported_date,
+        dispatched_resource,
+        dispatch_date,
+        start_for_fire_date,
+        assessment_resource,
+        assessment_datetime,
+        assessment_hectares,
+        fire_spread_rate,
+        fire_type,
+        fire_position_on_slope,
+        weather_conditions_over_fire,
+        temperature,
+        relative_humidity,
+        wind_direction,
+        wind_speed,
+        fuel_type,
+        initial_action_by,
+        ia_arrival_at_fire_date,
+        ia_access,
+        fire_fighting_start_date,
+        fire_fighting_start_size,
+        bucketing_on_fire,
+        distance_from_water_source,
+        first_bucket_drop_date,
+        bh_fs_date,
+        bh_hectares,
+        uc_fs_date,
+        uc_hectares,
+        to_fs_date,
+        to_hectares,
+        ex_fs_date,
+        ex_hectares
+
+    from source
+
+)
+
+select * from renamed
